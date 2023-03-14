75a60e6a11ab0dd80b2894fafe537fc363812a14
Ahmad Badran <ahmadbadran332211@gmail.com>
2023-03-14 10:20:44 +0300

    animated text added

--- scripts.js
+++ scripts.js
@@ -1,10 +1,8 @@
-$('.navTrigger').click(function () {
-    $(this).toggleClass('active');
-    console.log("Clicked menu");
-    $("#mainListDiv").toggleClass("show_list");
-    $("#mainListDiv").fadeIn();
-
+$(".navTrigger").click(function () {
+  $(this).toggleClass("active");
+  console.log("Clicked menu");
+  $("#mainListDiv").toggleClass("show_list");
+  $("#mainListDiv").fadeIn();
 });
 
-  AOS.init();
-
+AOS.init();
